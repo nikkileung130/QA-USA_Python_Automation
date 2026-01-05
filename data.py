e8493baef95ea819077ba17395c1a17d542893d5
nikki <nikkileung130@gmail.com>
2026-01-04 20:26:26 -0500

    initial commit

---
+++ data.py
@@ -0,0 +1,8 @@
+URBAN_ROUTES_URL ='https://cnt-ba959c32-1fff-4ad5-9421-85a46624dab3.containerhub.tripleten-services.com/'
+ADDRESS_FROM ='East 2nd Street, 601'
+ADDRESS_TO ='1300 1st St'
+PHONE_NUMBER ='+1 123 123 12 12'
+CARD_NUMBER	='1234 5678 9100'
+CARD_CODE ='1111'
+MESSAGE_FOR_DRIVER ='Stop at the juice bar, please'
+TEST = '1'
---
+++ main.py
@@ -0,0 +1,53 @@
+import data
+import helpers
+
+
+class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    def test_set_route(self):
+        #Add in S8
+        print('function created for set route')
+        pass
+
+    def test_select_plan(self):
+        #Add in S8
+        print('function created for select plan')
+        pass
+
+    def test_fill_phone_number(self):
+        #Add in S8
+        print('function created for fill phone number')
+        pass
+
+    def test_fill_card(self):
+        #Add in S8
+        print('function created for fill card')
+        pass
+
+    def test_comment_for_driver(self):
+        #Add in S8
+        print('function created for comment for driver')
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        #Add in S8
+        print('function created for order_blanket_and_handkerchiefs')
+        pass
+
+    def test_order_2_ice_creams(self):
+        #Add in S8
+        for _ in range(2):
+        #Add in S8
+        print('function created for order_2_ice_creams')
+        pass
+
+    def test_car_search_model_appears(self):
+        #Add in S8
+        print('function created for car_search_model_appears')
+        pass
